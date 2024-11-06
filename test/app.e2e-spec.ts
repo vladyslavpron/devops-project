75b63b96ab4ef28669f6398ddabdcb9490849032
vladyslavpron <vladyslavpron@gmail.com>
2024-11-06 22:14:28 +0200

    added deployment to workflow

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World from project1!');
+      .expect('Hello 🥰!');
   });
 });
